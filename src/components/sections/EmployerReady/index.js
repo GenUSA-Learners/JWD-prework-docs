3e0cc676c22ae6f17a54cedbe129979f0d9a2f10
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-07-11 13:36:18 -0400

    refactor: Move sections into folder

---
+++ src/components/sections/EmployerReady/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+import React from 'react';
+
+export default function EmployerReady () {
+	return (
+		<></>
+	);
+}
---
+++ src/components/sections/Plagarism/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+import React from 'react';
+
+export default function Plagarism () {
+	return (
+		<></>
+	);
+}
